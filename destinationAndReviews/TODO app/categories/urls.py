e061ef5c34b213bfcee7ba42642b0712552f050f
MonsterGuti <gogulanovmartin@gmail.com>
2026-01-26 16:55:57 +0200

    commit

--- destinationAndReviews/TODO app/categories/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from categories import views
-
-urlpatterns = [
-    path('', views.list_categories, name='list-categories'),
-]
--- destinationAndReviews/TODO app/notes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from notes.models import Note
-
-
-@admin.register(Note)
-class NoteAdmin(admin.ModelAdmin):
-    pass
