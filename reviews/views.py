6353c51fc5c2d5c1d7848eed371cdeab8022db4b
MonsterGuti <gogulanovmartin@gmail.com>
2026-02-13 22:13:22 +0200

    feat: added more solutions

---
+++ reviews/views.py
@@ -0,0 +1,11 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from reviews.forms import ReviewForm
+from reviews.models import Review
+
+
+class ReviewCreateView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    success_url = reverse_lazy('common:home')
